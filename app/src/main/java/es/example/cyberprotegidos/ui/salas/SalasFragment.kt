0f29ce1da8a3a49dc522e854f074a14314320dfc
alberto <alberto.melladomontes@alum.uca.es>
2023-05-18 20:39:01 +0200

    version

--- app/src/main/java/es/example/cyberprotegidos/ui/salas/SalasFragment.kt
+++ app/src/main/java/es/example/cyberprotegidos/ui/salas/SalasFragment.kt
@@ -28,10 +28,6 @@ class SalasFragment : Fragment() {
         _binding = FragmentSalasBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textGallery
-        galleryViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
         return root
     }
 
