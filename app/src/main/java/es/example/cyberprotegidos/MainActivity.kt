33bf38e54592611fbe360a46249436af159ff1bd
David Brea <david.breapinero@alum.uca.es>
2023-05-18 10:37:54 +0200

    Segunda versión de la app móvil de Cyberprotegidos

--- app/src/main/java/es/example/cyberprotegidos/MainActivity.kt
+++ app/src/main/java/es/example/cyberprotegidos/MainActivity.kt
@@ -1,7 +1,9 @@
 package es.example.cyberprotegidos
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
@@ -12,6 +14,9 @@ import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import es.example.cyberprotegidos.databinding.ActivityMainBinding
+import es.example.cyberprotegidos.ui.reservas.Reserva
+import es.example.cyberprotegidos.ui.reservas.ReservaAdapter
+import es.example.cyberprotegidos.ui.reservas.ReservasFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -37,11 +42,25 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_salas, R.id.nav_pets
+                R.id.nav_home, R.id.nav_salas, R.id.nav_reservas
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.nav_reservas-> {
+                // Aquí se ejecutará el código cuando se seleccione el elemento de menú "nav_reservas"
+                // Define la acción para abrir la actividad "Reserva"
+                val intent = Intent(this@MainActivity, ReservasFragment::class.java)
+                startActivity(intent)
+                return true
+            }
+            // Puedes manejar otros elementos de menú aquí si es necesario
+            else -> return super.onOptionsItemSelected(item)
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
