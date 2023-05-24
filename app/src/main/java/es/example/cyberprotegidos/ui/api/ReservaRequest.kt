1b58885412caa3ed4ae1281a3f5bdf8154753d11
David Brea <david.breapinero@alum.uca.es>
2023-05-24 19:05:24 +0200

    Version CASI final

--- app/src/main/java/es/example/cyberprotegidos/ui/api/ReservaRequest.kt
+++ app/src/main/java/es/example/cyberprotegidos/ui/api/ReservaRequest.kt
@@ -1,9 +1,9 @@
-package es.example.cyberprotegidos.ui.reservas
+package es.example.cyberprotegidos.ui.api
 
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class Reserva(val _id: String, val tipo: String, val nombre: String, val dni: String, val telefono: String,
+data class ReservaRequest(val tipo: String, val nombre: String, val dni: String, val telefono: String,
                           val email: String, val fechaIni: String, val fechaFin: String,
                           val numPersonas: String, val comentario: String){
 
